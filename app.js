c499326e5532e1e2590b9ce8ef60f2a193155479
Brittany Walker <breneewalker@gmail.com>
2017-06-11 12:27:17 -0400

    add javascript for sidebar menu

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("side-navigation").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("side-navigation").style.width = "0";
+}
