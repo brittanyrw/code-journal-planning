a0c890ee16771f9430cb48aa3a4dc78b6bef1294
Brittany Walker <breneewalker@gmail.com>
2017-07-06 00:30:48 -0400

    update jquery for html wireframes

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ $(function() {
 		$( ".side-navigation").removeClass( "open-side-navigation");
 	});
 
+	$(".subject-select").select2();
+
 });
+
+
+//tweet when something is completed
+//variable for page
+//take height off squares
+//breadcrumbs - dashboard category details
